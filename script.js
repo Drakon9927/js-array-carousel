bda61ddb989a4c0e780edce9446d690d6efd1dbd
Antonino <drakon9927@libero.it>
2024-04-23 16:04:49 +0200

    M2 - sistemare css

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
+document.getElementsByClassName("slide");
+document.querySelector("active");
+
+
 // Tutte le slides in un array
 let slides = document.getElementsByClassName("slide");
 
 let immagineAttiva;
 let prossimaSlide;
 
-for (let i = 0; index < slides.length; i++) {
+for (let i = 0; i < slides.length; i++) {
 
     const slide = slides[i];
     console.log(i, slide);
@@ -29,6 +33,6 @@ if( prossimaSlide >= slides.length ) {
     prossimaSlide = 0;
 }
 
-slides[prossimaSlide].classList.add("active");
+slides[prossimaSlide]
 
 console.log("L'immagine attiva era: ", immagineAttiva);
